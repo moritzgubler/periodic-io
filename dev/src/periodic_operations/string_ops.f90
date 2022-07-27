8a04812f5eb36297c814581faa34f2aa5631d144
moritzgubler <moritz.gubler@gmail.com>
2022-07-27 10:08:47 +0200

    added lower_case subroutine

--- dev/src/periodic_operations/string_ops.f90
+++ dev/src/periodic_operations/string_ops.f90
@@ -125,6 +125,18 @@ subroutine get_basename(filename, bname)
   bname = filename(slash_pos+1:point_pos - 1)
 end subroutine get_basename
 
+subroutine lower_case(word)
+!! convert a word to lower case
+character (len=*) , intent(in out) :: word
+integer                            :: i,ic,nlen
+nlen = len(word)
+do i=1,nlen
+   ic = ichar(word(i:i))
+   if (ic >= 65 .and. ic < 90) word(i:i) = char(ic+32)
+end do
+end subroutine lower_case
+
+
 !program test
 !  implicit none
 !  character(len=100) :: string
