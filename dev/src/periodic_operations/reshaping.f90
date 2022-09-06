1d8b58f186b5811e7d859747f9229050d2d1f29b
moritzgubler <moritz.gubler@gmail.com>
2022-09-06 12:05:59 +0200

    developed fast version of cell reshaping

--- dev/src/periodic_operations/reshaping.f90
+++ dev/src/periodic_operations/reshaping.f90
@@ -7,122 +7,51 @@ subroutine reshapecell(nat, alat0, rxyz)
   !! cartesian coordinates of atoms
   real*8, dimension(3, 3), intent(inout) :: alat0
   !! lattice matrix
-  real*8, dimension(3, 3) :: alat
-  !! lattice vectors
-  real*8, dimension(3, 3) :: alatmin
-  real*8 :: area, area1, area2, area3, areamin
-  integer :: i, j, imax, iba, ica, icb
-  real*8 :: vol, vol0
-  logical :: alatminset = .FALSE.
-  real(8) :: p(3, 3), alat_save(3, 3), pos0(3, nat), pos1(3, nat), pos2(3, nat), pinv(3, 3)
-  integer :: iperm
+  real(8) :: p(3, 3), pos_temp(3, nat), pinv(3, 3)
+  integer :: imax
+  logical :: success
 
   call alat2ascii(nat, rxyz, alat0)
-  pos0 = rxyz
-  pos1 = rxyz
-  pos2 = rxyz
-  alat_save = alat0
   imax = 6
-  vol0 = alat0(1,1) * alat0(2,2) * alat0(3, 3) 
-  areamin = 1.d100
-  alat(:, 1) = alat0(:, 1)
-  do iba = - imax, imax
-    alat(:, 2) = alat0(:, 2) + iba * alat0(:,1)
-    do ica = -imax, imax
-      do icb = -imax, imax
-        alat(:, 3) = alat0(:, 3) + ica * alat0(:, 1) + icb * alat0(:, 2)
-        vol = alat(1, 1) * alat(2, 2) * alat(3,3)
-        if (abs(vol - vol0) .lt. 1.d-8) then
-          call cellsurface(alat(1, 1), alat(1, 2), area1)
-          call cellsurface(alat(1, 1), alat(1, 3), area2)
-          call cellsurface(alat(1, 2), alat(1, 3), area3)
-          area = area1 + area2 + area3
-          if (area .lt. areamin) then
-            iperm = 0
-            alatminset = .TRUE.
-            areamin = area
-            do j = 1, 3; do i = 1, 3
-                alatmin(i, j) = alat(i, j)
-            end do; end do
-          end if
-        end if
-      end do
-    end do  
-  end do
 
+  ! try reshaping in original upper triangular lattice matrix
+  call reshape_ascii(alat0, imax, success)
+
+  ! search in first permutation of lattice vectors:
+  ! 1 -> 3
+  ! 2 -> 1
+  ! 3 -> 2
   p = 0.d0
   p(1, 2) = 1.d0
   p(2, 3) = 1.d0
   p(3, 1) = 1.d0
   call matinv3(p, pinv)
-  alat0 = matmul(alat_save, p)
-  call alat2ascii(nat, pos1, alat0)
-  vol0 = alat0(1,1) * alat0(2,2) * alat0(3, 3) 
-  alat(:, 1) = alat0(:, 1)
-  do iba = - imax, imax
-    alat(:, 2) = alat0(:, 2) + iba * alat0(:,1)
-    do ica = -imax, imax
-      do icb = -imax, imax
-        alat(:, 3) = alat0(:, 3) + ica * alat0(:, 1) + icb * alat0(:, 2)
-        vol = alat(1, 1) * alat(2, 2) * alat(3,3)
-        if (abs(vol - vol0) .lt. 1.d-8) then
-          call cellsurface(alat(1, 1), alat(1, 2), area1)
-          call cellsurface(alat(1, 1), alat(1, 3), area2)
-          call cellsurface(alat(1, 2), alat(1, 3), area3)
-          area = area1 + area2 + area3
-          if (area .lt. areamin) then
-            iperm = 1
-            alatminset = .TRUE.
-            areamin = area
-            alatmin = matmul(alat, pinv)
-          end if
-        end if
-      end do
-    end do  
-  end do
+  alat0 = matmul(alat0, p)
+  call alat2ascii(nat, rxyz, alat0)
+  call reshape_ascii(alat0, imax, success)
+  if ( success ) then
+    alat0 = matmul(alat0, pinv)
+  end if
+  call back2cell(nat, rxyz, alat0)
 
+  ! search in second permuation of lattice vectors:
+  ! 1 -> 2
+  ! 2 -> 3
+  ! 3 -> 1
+  pos_temp = rxyz 
   p = 0.d0
   p(1, 3) = 1.d0
   p(2, 1) = 1.d0
   p(3, 2) = 1.d0
   call matinv3(p, pinv)
-  alat0 = matmul(alat_save, p)
-  call alat2ascii(nat, pos2, alat0)
-  vol0 = alat0(1,1) * alat0(2,2) * alat0(3, 3) 
-  alat(:, 1) = alat0(:, 1)
-  do iba = - imax, imax
-    alat(:, 2) = alat0(:, 2) + iba * alat0(:,1)
-    do ica = -imax, imax
-      do icb = -imax, imax
-        alat(:, 3) = alat0(:, 3) + ica * alat0(:, 1) + icb * alat0(:, 2)
-        vol = alat(1, 1) * alat(2, 2) * alat(3,3)
-        if (abs(vol - vol0) .lt. 1.d-8) then
-          call cellsurface(alat(1, 1), alat(1, 2), area1)
-          call cellsurface(alat(1, 1), alat(1, 3), area2)
-          call cellsurface(alat(1, 2), alat(1, 3), area3)
-          area = area1 + area2 + area3
-          if (area .lt. areamin) then
-            iperm = 2
-            alatminset = .TRUE.
-            areamin = area
-            alatmin = matmul(alat, pinv)
-          end if
-        end if
-      end do
-    end do  
-  end do
-
-  if ( alatminset ) then
-    alat0 = alatmin
-  else
-    write(*, *) 'reshape cell would not have worked.'
-    write(666, *) 'reshape cell would not have worked.'
-    flush(666)
+  alat0 = matmul(alat0, p)
+  call alat2ascii(nat, pos_temp, alat0)
+  call reshape_ascii(alat0, imax, success)
+  if ( success ) then
+    rxyz = pos_temp
+    alat0 = matmul(alat0, pinv)
+    call back2cell(nat, rxyz, alat0)
   end if
-  if (iperm == 0) rxyz = pos0
-  if (iperm == 1) rxyz = pos1
-  if (iperm == 2) rxyz = pos2
-  call back2cell(nat, rxyz, alat0)
 
 contains
 
@@ -185,97 +114,43 @@ contains
     B(2, 3) = -detinv*(A(1, 1)*A(2, 3) - A(1, 3)*A(2, 1))
     B(3, 3) = +detinv*(A(1, 1)*A(2, 2) - A(1, 2)*A(2, 1))
   end subroutine matinv3
-  
-  subroutine cellsurface(a, b, area_in)
-    !! calculates surface spanned by two 3d vectors
-    implicit none
-    real*8, dimension(3) :: a
-    real*8, dimension(3) :: b
-    real*8, dimension(3) :: c
-    real*8 :: area_in
-
-    c(1) = a(2)*b(3) - b(2)*a(3)
-    c(2) = a(3)*b(1) - b(3)*a(1)
-    c(3) = a(1)*b(2) - b(1)*a(2)
-    area_in = sqrt(c(1)**2 + c(2)**2 + c(3)**2)
-  end subroutine cellsurface
 
 end subroutine reshapecell
 
-subroutine reshapecell_slow(nat, alat0, rxyz)
-  !! reshapes to cell to a more convenient shape if possible
+subroutine reshape_ascii(alat0, imax, new_lattice_found)
   implicit none
-  integer, intent(in) :: nat
-  !! number of atoms
-  real*8, dimension(3, nat), intent(inout) :: rxyz
-  !! cartesian coordinates of atoms
-  real*8, dimension(3, 3), intent(out) :: alat0
-  !! lattice matrix
-  real*8, dimension(3, 3) :: alat
-  !! lattice vectors
-  real*8, dimension(3, 3) :: alatmin
-  real*8 :: area, area1, area2, area3, areamin
-  integer :: i, i12, i13, i21, i23, i31, i32, j, imax
-  real*8 :: vol, vol0
-  logical :: alatminset = .FALSE.
-
-  imax = 6
-
-  vol0 = abs(alat0(1, 1)*alat0(2, 2)*alat0(3, 3) - alat0(1, 1)*alat0(2, 3)*alat0(3, 2) - &
-             alat0(1, 2)*alat0(2, 1)*alat0(3, 3) + alat0(1, 2)*alat0(2, 3)*alat0(3, 1) + &
-             alat0(1, 3)*alat0(2, 1)*alat0(3, 2) - alat0(1, 3)*alat0(2, 2)*alat0(3, 1))
-
-  areamin = 1.d100
-
-  do i13 = -imax, imax
-    do i12 = -imax, imax
-      alat(1, 1) = alat0(1, 1) + i12*alat0(1, 2) + i13*alat0(1, 3)
-      alat(2, 1) = alat0(2, 1) + i12*alat0(2, 2) + i13*alat0(2, 3)
-      alat(3, 1) = alat0(3, 1) + i12*alat0(3, 2) + i13*alat0(3, 3)
-      do i21 = -imax, imax
-        do i23 = -imax, imax
-          alat(1, 2) = alat0(1, 2) + i21*alat0(1, 1) + i23*alat0(1, 3)
-          alat(2, 2) = alat0(2, 2) + i21*alat0(2, 1) + i23*alat0(2, 3)
-          alat(3, 2) = alat0(3, 2) + i21*alat0(3, 1) + i23*alat0(3, 3)
-          do i31 = -imax, imax
-            do i32 = -imax, imax
-              alat(1, 3) = alat0(1, 3) + i31*alat0(1, 1) + i32*alat0(1, 2)
-              alat(2, 3) = alat0(2, 3) + i31*alat0(2, 1) + i32*alat0(2, 2)
-              alat(3, 3) = alat0(3, 3) + i31*alat0(3, 1) + i32*alat0(3, 2)
-
-              vol = abs(alat(1, 1)*alat(2, 2)*alat(3, 3) - alat(1, 1)*alat(2, 3)*alat(3, 2) - &
-                        alat(1, 2)*alat(2, 1)*alat(3, 3) + alat(1, 2)*alat(2, 3)*alat(3, 1) + &
-                        alat(1, 3)*alat(2, 1)*alat(3, 2) - alat(1, 3)*alat(2, 2)*alat(3, 1))
-
-              if (abs(vol - vol0) .lt. 1.d-8) then
-                call cellsurface(alat(1, 1), alat(1, 2), area1)
-                call cellsurface(alat(1, 1), alat(1, 3), area2)
-                call cellsurface(alat(1, 2), alat(1, 3), area3)
-                area = area1 + area2 + area3
-                if (area .lt. areamin) then
-                  alatminset = .TRUE.
-                  areamin = area
-                  do j = 1, 3; do i = 1, 3
-                      alatmin(i, j) = alat(i, j)
-                  end do; end do
-                end if
-              end if
-
-            end do
-          end do
-        end do
+  real(8), intent(inout) :: alat0(3, 3)
+  integer, intent(in) :: imax
+  logical, intent(out) :: new_lattice_found
+  real(8) :: vol0, alat(3, 3), vol, area, areamin
+  real(8) :: area1, area2, area3, alatmin(3, 3)
+  integer :: iba, ica, icb
+  new_lattice_found = .false.
+  vol0 = alat0(1,1) * alat0(2,2) * alat0(3, 3)
+  call cellsurface(alat0(1, 1), alat0(1, 2), area1)
+  call cellsurface(alat0(1, 1), alat0(1, 3), area2)
+  call cellsurface(alat0(1, 2), alat0(1, 3), area3)
+  areamin = area1 + area2 + area3
+  alat(:, 1) = alat0(:, 1)
+  do iba = - imax, imax
+    alat(:, 2) = alat0(:, 2) + iba * alat0(:,1)
+    do ica = -imax, imax
+      do icb = -imax, imax
+        alat(:, 3) = alat0(:, 3) + ica * alat0(:, 1) + icb * alat0(:, 2)
+        vol = alat(1, 1) * alat(2, 2) * alat(3,3)
+        call cellsurface(alat(1, 1), alat(1, 2), area1)
+        call cellsurface(alat(1, 1), alat(1, 3), area2)
+        call cellsurface(alat(1, 2), alat(1, 3), area3)
+        area = area1 + area2 + area3
+        if (area .lt. areamin) then
+          new_lattice_found = .true.
+          areamin = area
+          alatmin = alat 
+        end if
       end do
-    end do
+    end do  
   end do
-  if ( alatminset ) then
-    alat0 = alatmin
-  else
-    write(*, *) 'reshape cell would not have worked.'
-    write(666, *) 'reshape cell would not have worked.'
-    flush(666)
-  end if
-
-  call back2cell(nat, rxyz, alat0)
+  alat0 = alatmin 
 
 contains
 
@@ -293,4 +168,4 @@ contains
     area_in = sqrt(c(1)**2 + c(2)**2 + c(3)**2)
   end subroutine cellsurface
 
-end subroutine reshapecell_slow
+end subroutine reshape_ascii
